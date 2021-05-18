24a7356fcc14ef670b4aa8fa7ad56822750928c6
racheljamesusdot <84399752+racheljamesusdot@users.noreply.github.com>
2021-05-18 11:46:25 -0400

    Delete Read_data.m

--- Read_data.m
+++
@@ -1,7 +0,0 @@
-function [data] = Read_data(path)
-%this function is used to read data from a csv file.
-%format of the csv file should be consistent with the format showed in
-%the data_description.docx.
-data = csvread(path,1,0);
-data = sortrows(data,1,'ascend');
-end
